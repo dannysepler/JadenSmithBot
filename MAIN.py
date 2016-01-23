1efed10c97d997ad539b8c2b7e4b0a5951fb19aa
Danny Sepler <dsepler@ufl.edu>
2016-01-23 15:45:50 -0500

    made my first tweet! :')

--- MAIN.py
+++ MAIN.py
@@ -1,1 +1,35 @@
-print 'hello, jaden'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+import tweepy, json
+
+print 'starting'
+
+# load in secrets
+print 'loading secrets'
+
+secrets_file = open('secrets.json', 'r')
+secrets_decode = json.load(secrets_file)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# load in the twitter data
+print 'loading twitter data'
+
+CONSUMER_KEY = secrets_decode["twitter"]["consumer_key"]
+CONSUMER_SECRET = secrets_decode["twitter"]["consumer_secret"]
+ACCESS_KEY = secrets_decode["twitter"]["access_key"]
+ACCESS_SECRET = secrets_decode["twitter"]["access_secret"]
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# do things
+print 'doing things'
+
+api.update_status("i'm alive!!")
+
+print 'done'
